455e7c27d607968c9e1d51e40844589cb3fa417e
JPHHacks <jpheymann@protonmail.com>
2026-02-18 16:08:32 -0500

    updated resume: google drive link

--- terraform/modules/sns/variables.tf
+++ terraform/modules/sns/variables.tf
@@ -2,7 +2,7 @@ variable "environment" {
   description = "Environment name"
   type        = string
 }
-
+ 
 variable "email_address" {
   description = "Email address for security alerts"
   type        = string
